26c466f23a5686403d38b1013629e9732ada94be
Bartosz Szplet <bartosz300@gmail.com>
2026-08-19 16:45:50 +0200

    Change kernel config download URL for raphael
    Updated the kernel configuration file URL to the latest version.

--- raphael-kernel_build.sh
+++ raphael-kernel_build.sh
@@ -12,7 +12,7 @@ git add .
 git commit -m "builddeb: Add Qcom SM8150 DTBs to boot partition"
 
 # 下载内核配置文件
-wget -O arch/arm64/configs/raphael.config https://raw.githubusercontent.com/GengWei1997/kernel-deb/refs/heads/main/uboot-raphael.config
+wget -O arch/arm64/configs/raphael.config https://raw.githubusercontent.com/GengWei1997/kernel-deb/refs/heads/7.2/raphael.config
 
 echo "CONFIG_ISCSI_TCP=y" >> arch/arm64/configs/raphael.config
 echo "CONFIG_ZRAM_BACKEND_LZ4=y" >> arch/arm64/configs/raphael.config
